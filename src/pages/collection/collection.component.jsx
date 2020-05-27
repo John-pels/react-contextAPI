3389b7ec6bbf657cc05e51b2cf3b2f70c078d61b
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-05-27 06:53:28 -0400

    I switch from redux to ContextAPI

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionsContexts from "../../contexts/collections/collections.context";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import "./collection.styles.scss";
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import './collection.styles.scss';
-
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContexts);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
@@ -21,8 +20,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
